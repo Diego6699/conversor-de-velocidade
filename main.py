0d8084ac337559c77850a900b2945859a80adbf8
DIEGO LEONARDO DA SILVA FRANÇA <diegoleonardo9966@gmail.com>
2022-09-01 15:59:44 -0300

    tela de login simples

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-from cgitb import text
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivymd.uix.boxlayout import BoxLayout
 
-def on_press(btn):
-    btn.text = "apertado"
-def on_release(btn):
-    btn.text = "solto"
-class MyApp(MDApp):
+class MyBoxLayout(BoxLayout):
+    pass
+class MainApp(MDApp):
     def build(self):
-        box = BoxLayout()
-        box.orientation = "vertical"
-        #label
-        label = MDLabel()
-        label.text = "ola mundo"
-        label.font_size = 50
-        #input
-        text_input = TextInput(
-
-        )
-        #buttao
-        btn = Button(
-            text="botao",
-            font_size=50,
-            on_press=on_press,
-            on_release=on_release
-        )
-        box.add_widget(label)
-        box.add_widget(text_input)
-        box.add_widget(btn)
-        return box
-MyApp().run()
+        self.theme_cls.theme_style = "Dark"
+        return MyBoxLayout()
+MainApp().run()
